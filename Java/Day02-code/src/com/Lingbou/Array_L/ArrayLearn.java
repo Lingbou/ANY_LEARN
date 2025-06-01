0b3ff716abb2862559f6d61b03580cfb05dfb52b
Lingbou <2180717380@qq.com>
2025-06-01 20:23:49 +0800

    Java learn

---
+++ Java/Day02-code/src/com/Lingbou/Array_L/ArrayLearn.java
@@ -0,0 +1,22 @@
+package com.Lingbou.Array_L;
+
+public class ArrayLearn {
+    public static void main(String[] args) {
+        // 1
+        int[] arr1 = new int[]{1, 2, 3, 4};
+        int[] arr2 = {1, 2, 3, 4};
+
+        //2
+        String[] arr3 = new String[]{"Fuck", "You"};
+        String[] arr4 = {"Fuck", "You"};
+
+        //3
+        int arr5[] = {1,2,3,4,5};
+        for(int i = 0; i < arr5.length; i++){
+//            System.out.println(arr5[i]);
+            System.out.printf("%d ",arr5[i]);
+        }
+        System.out.printf("\n");
+
+    }
+}
