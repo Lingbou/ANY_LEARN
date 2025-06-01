0d14aa38cd1bc03995a30d6c76e4f553b433cdd9
Lingbou <2180717380@qq.com>
2025-06-01 14:13:17 +0800

    Java learn

---
+++ Java/BasicDataType.java
@@ -0,0 +1,26 @@
+/**
+ * Print basic data-type
+ * 
+ * @Lingbou
+ * @version 1.0
+ */
+public class BasicDataType {
+    /**
+     * A SHIT
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(666);
+        System.out.println(6.66);
+        System.out.println("FUCK YOU");
+        System.out.println('F');
+        System.out.println('U');
+        System.out.println('C');
+        System.out.println('K');
+        System.out.println(true);
+        System.out.println(false);
+
+        // What are you doing? null can't be println print.
+        // System.out.println(null);        
+    }
+}
--- Java/helloworld.java
+++ Java/helloworld.java
@@ -1,4 +1,14 @@
+/**
+ * 这是一坨答辩
+ * 
+ * @Lingbou
+ * @version 1.0
+ */
 public class helloworld {
+    /**
+     * 打印Hello world
+     * @param args 并没有什么卵用的数组
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
