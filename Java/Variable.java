829c3cb6bb1bb542c45d406ee25ba1fb0a94090b
Lingbou <2180717380@qq.com>
2025-06-01 14:39:29 +0800

    Java learn

--- Java/Variable.java
+++ Java/Variable.java
@@ -8,7 +8,6 @@ public class Variable {
     public static void main(String[] args) {
         int a = 666;
         System.out.println(a);
-
         int b = 344;
         System.out.println(a + b);
 
@@ -16,7 +15,18 @@ public class Variable {
         char d = 'u';
         char e = 'c';
         char f = 'k';
-
         System.out.printf("%s\n", "" + c + d + e +f);
+
+        // Long int
+        long long_num = 9999999999999L;
+        System.out.println(long_num);
+
+        // float
+        float float_num = 3.14F;
+        System.out.println(float_num);
+
+        //double
+        double double_num = 3.14;
+        System.out.println(double_num);
     }
 }
