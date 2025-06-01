f8cdb21d65b5818b248d8d4a1c1ace4eb5c70b6a
Lingbou <2180717380@qq.com>
2025-06-01 14:17:55 +0800

    Java learn

---
+++ Java/Tablechar.java
@@ -0,0 +1,18 @@
+/**
+ * "\t" 用法
+ * 
+ * @Lingbou
+ * @version 1.0
+ */
+public class Tablechar {
+    /**
+     * A SHIT
+     * @param args
+     */
+    public static void main(String[] args) {
+            System.out.println("name" + '\t' + "age");
+            System.out.println("Tom" + '\t' + "12");
+            System.out.println("Fuck" + '\t' + "6");
+
+    }
+}
