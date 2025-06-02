94d8dc12cb4d05787d26178ebb57220961dea728
Lingbou <2180717380@qq.com>
2025-06-02 20:54:44 +0800

    Java learn

---
+++ Java/Day03-code/src/com/Lingbou/Method/MethodLearn.java
@@ -0,0 +1,10 @@
+package com.Lingbou.Method;
+
+public class MethodLearn {
+    public static void main(String[] args) {
+        sol(1, 1);
+    }
+    public static void sol(int a,int b){
+        System.out.println(a+b);
+    }
+}
