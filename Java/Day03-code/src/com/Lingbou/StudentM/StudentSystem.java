7da3e398f5fe66c94f3c2f4055de7d0a0517e95b
Lingbou <2180717380@qq.com>
2025-06-04 20:58:19 +0800

    Java learn

--- Java/Day03-code/src/com/Lingbou/StudentM/StudentSystem.java
+++ Java/Day03-code/src/com/Lingbou/StudentM/StudentSystem.java
@@ -1,10 +1,11 @@
 package com.Lingbou.StudentM;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StudentSystem {
     public static void main(String[] args) {
-
+        ArrayList<Student> list = new ArrayList<>();
         while(true){
             System.out.println("-------------Fuck You!-------------");
             System.out.println("1:添加学生");
@@ -19,10 +20,10 @@ public class StudentSystem {
             String choose = sc.next();
 
             switch(choose) {
-                case "1" -> System.out.println("添加学生");
-                case "2" -> System.out.println("删除学生");
-                case "3" -> System.out.println("修改学生");
-                case "4" -> System.out.println("查询学生");
+                case "1" -> addStudent(list);
+                case "2" -> deleteStudent(list);
+                case "3" -> updateStudent(list);
+                case "4" -> queryStudent(list);
                 case "5" -> {
                     System.out.println("退出");
                     System.exit(0);
@@ -31,4 +32,29 @@ public class StudentSystem {
             }
         }
     }
+
+
+    //添加学生
+    public static void addStudent(ArrayList<Student> list){
+        System.out.println("添加学生");
+    }
+
+    //删除学生
+    public static void deleteStudent(ArrayList<Student> list){
+        System.out.println("删除学生");
+    }
+
+    //修改学生
+    public static void updateStudent(ArrayList<Student> list){
+        System.out.println("修改学生");
+    }
+
+    //查询学生
+    public static void queryStudent(ArrayList<Student> list){
+        System.out.println("查询学生");
+    }
+
+
+
+
 }
