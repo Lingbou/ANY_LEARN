4a03879ff20ae945c267f3de968c4a5f3d297bb1
Lingbou <2180717380@qq.com>
2025-06-26 17:01:21 +0800

    Java Learn

--- Java/Day06-code/src/com/Lingbou/ui/GameJFrame.java
+++ Java/Day06-code/src/com/Lingbou/ui/GameJFrame.java
@@ -4,15 +4,49 @@ import javax.swing.*;
 
 public class GameJFrame extends JFrame {
     public GameJFrame() {
+        initJFrame();
+
+        initJMenuBar();
+
+        this.setVisible(true);
+    }
+
+    private void initJMenuBar() {
+        // 创建菜单
+        JMenuBar jMenuBar = new JMenuBar();
+
+
+        JMenu functionJMenu = new JMenu("功能");
+        JMenu aboutJMenu = new JMenu("关于我们");
+
+
+        JMenuItem replayItem = new JMenuItem("重新游戏");
+        JMenuItem reLoginItem = new JMenuItem("重新登录");
+        JMenuItem closeItem = new JMenuItem("关闭游戏");
+
+        JMenuItem accountItem = new JMenuItem("公众号");
+
+
+        functionJMenu.add(replayItem);
+        functionJMenu.add(reLoginItem);
+        functionJMenu.add(closeItem);
+
+        aboutJMenu.add(accountItem);
+
+
+        jMenuBar.add(functionJMenu);
+        jMenuBar.add(aboutJMenu);
+
+
+        this.setJMenuBar(jMenuBar);
+    }
+
+    private void initJFrame() {
         this.setSize(603, 680);
         this.setTitle("FUCK YOU");
         this.setAlwaysOnTop(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
-
-        this.setVisible(true);
     }
 
 
